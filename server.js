94a8ad5d834db8b40aba9eeedb076278221753e0
Nikhil Taneja <nikhiltaneja121@gmail.com>
2020-09-23 02:25:01 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express();
+const ejs = require('ejs');
+const path = require('path');
+const expressLayout = require('express-ejs-layouts')
+const PORT = process.env.PORT || 3300
+
+
+
+
+
+app.get('/' , (req ,res) => {
+
+    res.render('index');
+
+})
+
+//Set Template engine
+
+app.use(expressLayout);
+app.set('views' , path.join(__dirname , '/resources/views'))
+app.set('view engine', 'ejs')
+
+
+
+app.listen(PORT, () =>{
+    console.log(`Server is listening on port ${PORT}`)
+})
